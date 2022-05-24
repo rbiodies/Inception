40b3d85e2ec5f4273bc4f404fa3248fc766ceb3a
rbiodies <rbiodies@debian.rbiodies.42.fr>
2022-05-24 17:51:06 +0300

    do_comments

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -4,7 +4,8 @@
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
 # -R рекурсивное изменение прав доступа для каталогов и их содержимого
 chmod -R 775 /var/www/html/wordpress;
-# Следующий пример изменит владельца всех файлов и подкаталогов в /var/www/html/wordpress каталоге на нового владельца и группу с именем www-data :
+# Следующий пример изменит владельца всех файлов и 
+# подкаталогов в /var/www/html/wordpress каталоге на нового владельца и группу с именем www-data :
 chown -R www-data /var/www/html/wordpress;
 mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
@@ -13,18 +14,20 @@ touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 echo "Wordpress: setting up..."
-# После проверки требований загрузите файл wp-cli.phar , используя wgetили curl:
+# После проверки требований загрузите файл wp-cli.phar , используя wget или curl:
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-# Чтобы использовать WP-CLI из командной строки, набрав wp, сделайте файл исполняемым и переместите его куда-нибудь в PATH. Например:
 # «+x» означает — установить разрешение на запуск (x) файла для всех пользователей.
 chmod +x wp-cli.phar;
+# Чтобы использовать WP-CLI из командной строки, набрав wp, сделайте файл исполняемым и 
+# переместите его куда-нибудь в PATH. Например:
 mv wp-cli.phar /usr/local/bin/wp;
 cd /var/www/html/wordpress;
 # Загружает и извлекает основные файлы WordPress по указанному пути
 wp core download --allow-root;
 mv /var/www/wp-config.php /var/www/html/wordpress;
 echo "Wordpress: creating users..."
-# Создает таблицы WordPress в базе данных, используя URL-адрес, заголовок и предоставленные данные пользователя-администратора по умолчанию
+# Создает таблицы WordPress в базе данных, 
+# используя URL-адрес, заголовок и предоставленные данные пользователя-администратора по умолчанию
 # --url=<адрес>
 # Адрес нового сайта.
 # --title=<название сайта>
@@ -42,7 +45,8 @@ wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRESS_NAME} --ad
 # <user-email>
 # Адрес электронной почты пользователя для создания.
 # [--role=<role>]
-# Роль пользователя для создания. По умолчанию: роль по умолчанию. Возможные значения включают «администратор», «редактор», «автор», «участник», «подписчик».
+# Роль пользователя для создания. По умолчанию: роль по умолчанию. 
+# Возможные значения включают «администратор», «редактор», «автор», «участник», «подписчик».
 # [--user_pass=<password>]
 # Пароль пользователя. По умолчанию: генерируется случайным образом
 wp user create ${MYSQL_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root;
